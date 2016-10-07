9e3c7594a5ce37f38a2325c0570eeddd59a23845
padu <padu@merlotti.com>
2016-10-07 14:00:44 -0700

    This is the real splash screen, before it was just the gitignore

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,34 @@ namespace sideline
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(HandleUnhandledException);
+
+
+            var splashView = new SplashView();
+
+            splashView.WindowStartupLocation = WindowStartupLocation.Manual;
+            splashView.Show();
+
+            Thread.Sleep(2000);
+
+            var mainView = new MainWindow();
+
+            splashView.Close();
+
+
+            mainView.Show();
+
+        }
+
+        void HandleUnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var ex = (e.ExceptionObject as Exception);
+            MessageBox.Show("Pardon our dust... something went wrong.\n" + ex.Message + "\n" + ex.StackTrace,
+                            "Unhandled Error", MessageBoxButton.OK, MessageBoxImage.Exclamation);
+        }
+
     }
 }
